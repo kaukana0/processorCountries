84d21c58973fa61c5387068432ae3d550db40550
robert <kaukana0@tuta.io>
2022-06-12 22:19:00 +0200

    introduce change of group

--- processor.mjs
+++ processor.mjs
@@ -1,5 +1,9 @@
 /*
-this extracts and orders the countries.
+this 
+1) extracts and orders the countries,
+2) extracts the info after which countries there's a "change of group".
+   for instance, this can serve as a hint for a dropdown box to insert separators
+
 
 ordering:
 
@@ -14,12 +18,22 @@ logging - "A from input dropped, it's not in reference"
 */
 
 function order(input, ref) {
-    let out = new Map()
+    let out = new Map()         // ordered by insertion
+    let groupChanges = []
+    let lastElementKey
+
     ref.forEach(r => {
         if(input.hasOwnProperty(r)) {
             out.set(r, input[r])
+            lastElementKey = r
         } else {
-            console.warn("countryProcessor: '" + r + "' from input data dropped, because missing in reference")
+            if(r === "-") {
+                if(lastElementKey) {
+                    groupChanges.push(lastElementKey)
+                }
+            } else {
+                console.warn("countryProcessor: '" + r + "' from input data dropped, because missing in reference")
+            }
         }
     })
     // to aid possible incosistencies, check the other way round as well
@@ -29,10 +43,10 @@ function order(input, ref) {
         }
     }
 
-    return out
+    return [out, groupChanges]
 }
 
 // ccode : name  (ISO 3166-1 Alpha-2 codes (uppercase))
 export function process(inputData, output) {
-    output.countries = order(inputData.dimension.geo.category.label, output.countryOrder)
+    [output.countries, output.groupChanges] = order(inputData.dimension.geo.category.label, output.countryOrder)
 }
